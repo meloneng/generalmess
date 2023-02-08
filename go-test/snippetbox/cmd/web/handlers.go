e4193734004531b6df3282efbc5d612fa918b42e
meloneng <meloneng@protonmail.com>
2023-02-08 13:21:59 -0300

    Updated homepage, Started studying Elm

--- go-test/snippetbox/cmd/web/handlers.go
+++ go-test/snippetbox/cmd/web/handlers.go
@@ -17,6 +17,7 @@ func home(w http.ResponseWriter, r *http.Request){
     
     files := []string{
         "./ui/html/bookOnes/base.tmpl.html",
+        "./ui/html/bookOnes/partials/nav.tmpl.html",
         "./ui/html/bookOnes/pages/home.tmpl.html",
     }
 
