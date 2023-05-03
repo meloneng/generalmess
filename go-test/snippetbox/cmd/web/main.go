6d8073f7ad5c273858d46ce828817f91e6548e3a
meloneng <melon_eng@protonmail.com>
2023-05-03 19:02:45 -0300

    end of chapter 2

--- go-test/snippetbox/cmd/web/main.go
+++ go-test/snippetbox/cmd/web/main.go
@@ -9,11 +9,16 @@ import (
 
 func main(){
     mux := http.NewServeMux()
+
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
     mux.HandleFunc("/", home)
     mux.HandleFunc("/snippet/view", snippetView)
     mux.HandleFunc("/snippet/create", snippetCreate)
 
-    log.Println("Startgins server on :4000")
+    log.Println("Starting server on :4000")
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
 }
