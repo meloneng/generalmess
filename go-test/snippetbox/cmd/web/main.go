fe9d7813c5ca4be489ef6f2aca0e97311aed73e8
meloneng <melon_eng@protonmail.com>
2023-05-22 16:47:46 -0300

    dockerfile

--- go-test/snippetbox/cmd/web/main.go
+++ go-test/snippetbox/cmd/web/main.go
@@ -1,13 +1,18 @@
 package main
 
-
 import (
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
 )
 
 
 func main(){
+
+    addr := flag.String("addr", ":4000", "HTTP network address")
+    flag.Parse()
+
+
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -18,7 +23,7 @@ func main(){
     mux.HandleFunc("/snippet/view", snippetView)
     mux.HandleFunc("/snippet/create", snippetCreate)
 
-    log.Println("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
+    log.Printf("Starting server on %s", *addr)
+    err := http.ListenAndServe(*addr, mux)
     log.Fatal(err)
 }
